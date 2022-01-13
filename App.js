51952d534654924a7d69662e009f54aa11da2c3c
ryuzo111 <yajima0401ryuzo@gmail.com>
2022-01-13 15:56:26 +0900

    styleSheetを追加

--- App.js
+++ App.js
@@ -112,20 +112,50 @@
 // export default App;
 
 import React,{Component} from 'react';
-import { Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default class App extends Component {
   render() {
     let message = 'Hello World';
     return (
-      <View>
-        <Text style={{padding:50, textAlign: 'center', paddingTop:400}}>
+      <View style={styles.base}>
+        <Text style={styles.title}>
           {message}
         </Text>
-        <Text>
+        <Text style={styles.subtitle}>
           {message}
+        </Text>
+        <Text style={styles.message}>
+          This is sample message.
+          これは、サンプルメッセージです。
         </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({ 
+  base: {
+    padding:30
+  },
+  title: {
+    padding: 10,
+    color: 'red',
+    textAlign:'center',
+    fontSize:60,
+    fontWeight:'bold'
+  },
+  subtitle: {
+    padding:10,
+    color:'blue',
+    fontSize:20,
+    fontWeight:'bold'
+  },
+  message: {
+    padding:10,
+    color:'green',
+    fontSize:26,
+    lineHeight:50
+  }
+
+});
